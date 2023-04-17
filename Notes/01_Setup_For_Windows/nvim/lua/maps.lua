e395d99251a8432411e0bb7f7de7c5c6296d0d3b
Roman-Ojha <razzroman98@gmail.com>
2023-04-18 00:15:57 +0545

    feat: improved mapping

--- Notes/01_Setup_For_Windows/nvim/lua/maps.lua
+++ Notes/01_Setup_For_Windows/nvim/lua/maps.lua
@@ -2,6 +2,25 @@ local function map(mode, lhs, rhs)
 	vim.keymap.set(mode, lhs, rhs, { silent = true })
 end
 
+-- map("<mode>", "<key_to_map>", "<command_to_execute>")
+-- <mode> = n, v, i, c, t
+-- 		n = normal
+-- 		v = visual
+-- 		i = insert
+-- 		c = command
+-- 		t = terminal
+-- <key_to_map> = the key to map
+-- 		<leader> = the space key
+-- 		    <leader>ff = <space> + f + f
+-- 		<TAB> = the tab key
+-- 		<S-TAB> = the shift + tab key
+-- 		<C-h> = the control + h key
+-- 		<C-l> = the control + l key 
+-- 		<Space> = the space key
+-- <command_to_execute> = the command to execute
+
+-- Some more key binding examples: https://neovim.discourse.group/t/how-can-i-map-ctrl-shift-f5-ctrl-shift-b-ctrl-and-alt-enter/2133/2
+
 local status, telescope = pcall(require, "telescope.builtin")
 if status then
 	-- Telescope
@@ -18,33 +37,42 @@ end
 -- <leader> = the space key
 
 -- Save
-map("n", "<leader>w", "<CMD>update<CR>")
+map("n", "<C-s>", "<CMD>update<CR>")
 
 -- Quit
-map("n", "<leader>q", "<CMD>q<CR>")
+map("n", "<SA-w>", "<CMD>q<CR>")
+-- map to close all windows
+map("n", "<CA-w>", "<CMD>qall<CR>")
 
 -- Exit insert mode
-map("i", "jk", "<ESC>")
+-- map("i", "jk", "<ESC>")
 
--- Windows
-map("n", "<leader>ñ", "<CMD>vsplit<CR>")
-map("n", "<leader>p", "<CMD>split<CR>")
+-- Split Windows
+map("n", "<C-\\>", "<CMD>vsplit<CR>")
+map("n", "<CS-\\>", "<CMD>split<CR>")
 
 -- NeoTree
-map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
-map("n", "<leader>o", "<CMD>Neotree focus<CR>")
+map("n", "<SA-b>", "<CMD>Neotree toggle<CR>")
+map("n", "<C-0>", "<CMD>Neotree focus<CR>")
+-- map to create a new file using NeoTree
+map("n", "<SA-n>", "<CMD>enew<CR>")
 
 -- Buffer
 map("n", "<TAB>", "<CMD>bnext<CR>")
 map("n", "<S-TAB>", "<CMD>bprevious<CR>")
 
 -- Terminal
-map("n", "<leader>th", "<CMD>ToggleTerm size=10 direction=horizontal<CR>")
-map("n", "<leader>tv", "<CMD>ToggleTerm size=80 direction=vertical<CR>")
+map("n", "<CA-b>", "<CMD>ToggleTerm size=40 direction=vertical<CR><C-\\><C-n>", { noremap = true })
+-- Ctrl + Alt + b
+map("n", "<CA-n>", "<CMD>ToggleTerm size=10 direction=horizontal<CR><C-\\><C-n>", { noremap = true })
+-- map to exit terminal mode
+map("t", "<C-[>", "<C-\\><C-n>")
+-- map to focus terminal
+
 
 -- Markdown Preview
 map("n", "<leader>m", "<CMD>MarkdownPreview<CR>")
-map("n", "<leader>mn", "<CMD>MarkdownPreviewStop<CR>")
+map("n", "<leader>ms", "<CMD>MarkdownPreviewStop<CR>")
 
 -- Window Navigation
 map("n", "<C-h>", "<C-w>h")
@@ -53,7 +81,7 @@ map("n", "<C-k>", "<C-w>k")
 map("n", "<C-j>", "<C-w>j")
 
 -- Resize Windows
-map("n", "<C-Left>", "<C-w><")
-map("n", "<C-Right>", "<C-w>>")
-map("n", "<C-Up>", "<C-w>+")
-map("n", "<C-Down>", "<C-w>-")
+map("n", "<CA-h>", "<C-w><")
+map("n", "<CA-l>", "<C-w>>")
+map("n", "<CA-j>", "<C-w>+")
+map("n", "<CA-k>", "<C-w>-")
