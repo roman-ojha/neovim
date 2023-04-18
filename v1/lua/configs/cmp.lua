74374862515792e611fc7cd68997415b6f6d6993
Roman-Ojha <razzroman98@gmail.com>
2023-04-18 20:21:05 +0545

    feat: added all the stuff that i did inside v1

--- v1/lua/configs/cmp.lua
+++ v1/lua/configs/cmp.lua
@@ -38,7 +38,6 @@ local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   return
 end
-vim.notify("Hello roman")
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
   return
