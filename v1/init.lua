23bbe2a76c89d037c3720aec3879c619e3f24eaf
Roman-Ojha <razzroman98@gmail.com>
2023-04-19 12:50:22 +0545

    fix: moving nvim config to another repo

--- v1/init.lua
+++
@@ -1,7 +0,0 @@
--- to require './lua' folder files we don't have to write the full path, it is by default in the runtimepath
-require("settings")
-require("maps")
-require("plugins")
-require("colorscheme")
-require("terminal_emulator")
-require("configs.lsp")
--- v1/lua/terminal_emulator.lua
+++
@@ -1,3 +0,0 @@
--- vim.opt.shell ="cmd.exe" -- this can't open powershell another other terminals
--- Setting default terminal
-vim.g.terminal_emulator='pwsh'
